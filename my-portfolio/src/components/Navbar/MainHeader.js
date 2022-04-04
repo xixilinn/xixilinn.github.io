d0dfb404a60ec1ba1bc093018ef477f01ee320e2
Xi Xi Lin <xixi.lin1990@gmail.com>
2022-04-04 15:03:15 -0400

    update

--- my-portfolio/src/components/Navbar/MainHeader.js
+++ my-portfolio/src/components/Navbar/MainHeader.js
@@ -6,13 +6,13 @@ const MainHeader = () => {
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/portfolio">Xi Xi Lin</Navbar.Brand>
+                    <Navbar.Brand href="/">Xi Xi Lin</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto">
                         </Nav>
                         <Nav>
-                            <Nav.Link href="/portfolio">Home</Nav.Link>
+                            <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/projects">Projects</Nav.Link>
                             <Nav.Link href="/contact">
                                 Contact
